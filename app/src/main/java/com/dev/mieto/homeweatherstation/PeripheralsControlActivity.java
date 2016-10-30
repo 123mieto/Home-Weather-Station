97a232915b8134c4a3b503dd8888683f48f927fc
Marcin Mik <piptrik@poczta.fm>
2016-10-30 09:58:46 +0100

    Added Led control

--- app/src/main/java/com/dev/mieto/homeweatherstation/PeripheralsControlActivity.java
+++ app/src/main/java/com/dev/mieto/homeweatherstation/PeripheralsControlActivity.java
@@ -8,6 +8,10 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -18,14 +22,23 @@ public class PeripheralsControlActivity extends AppCompatActivity {
 
     public static final String TAG = MeasActivity.class.getSimpleName();
     public static final String ENDPOINT = "http://192.168.0.87:5000/api/v1/";
+    public static final String ENDPOINT_ESP8266 = "http://192.168.0.80:80/";
 
     private TextView mTvLed;
+    private TextView mTvESPLed;
     private Switch mSwLed;
+    private Switch mESPLEd;
 
     private int ledNumber;
     private long time, duration, deviceNumber;
 
     private RestService mRestService;
+    private OkHttpClient okClient;
+    //TODO: pomiar - inny plik
+//    private Request tempReqESP;
+//    private Request lightReqESP;
+    private Request ledOnESP;
+    private Request ledOffESP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,8 @@ public class PeripheralsControlActivity extends AppCompatActivity {
 
         mTvLed = (TextView) findViewById(R.id.tvLedSwith);
         mSwLed = (Switch) findViewById(R.id.swLed1);
+        mTvESPLed = (TextView) findViewById(R.id.tvESPLed);
+        mESPLEd = (Switch) findViewById(R.id.swLed2);
 
         //STUB
         ledNumber = 1;
@@ -42,6 +57,32 @@ public class PeripheralsControlActivity extends AppCompatActivity {
         deviceNumber = 1;
 
         prepareRetrofit();
+        prepareOkHttpClient();
+    }
+
+    private void prepareOkHttpClient() {
+        okClient = new OkHttpClient();
+        ledOnESP = new Request.Builder().url(ENDPOINT_ESP8266 + "ledOn").build();
+        ledOffESP = new Request.Builder().url(ENDPOINT_ESP8266 + "ledOff").build();
+        /*Request to check connection with esp*/
+        okClient.newCall(new Request.Builder().url(ENDPOINT_ESP8266).build()).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                mESPLEd.setEnabled(false);
+                mTvESPLed.setEnabled(false);
+                Toast.makeText(PeripheralsControlActivity.this, "Error connecting ESP8266", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+                mESPLEd.setEnabled(true);
+                mTvESPLed.setEnabled(true);
+            }
+        });
+
+        //TODO: powinno byc w innym pliku a nie w kontrol - to jest pobieranie pomiaru
+//        tempReqESP = new Request.Builder().url(ENDPOINT_ESP8266 + "temp").build();
+//        lightReqESP = new Request.Builder().url(ENDPOINT_ESP8266 + "light_lvl").build();
     }
 
     private void prepareRetrofit() {
@@ -60,7 +101,7 @@ public class PeripheralsControlActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<ConnHwResult> call, Throwable t) {
                 Log.e(TAG, "onFailure: NI MA NETU " + t.getMessage(), t);
-                Toast.makeText(PeripheralsControlActivity.this, "Error downloading", Toast.LENGTH_LONG).show();
+                Toast.makeText(PeripheralsControlActivity.this, "Error connecting raspberry", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -82,7 +123,7 @@ public class PeripheralsControlActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<ConnHwResult> call, Throwable t) {
                 Log.e(TAG, "onFailure: NI MA NETU " + t.getMessage(), t);
-                Toast.makeText(PeripheralsControlActivity.this, "Error downloading", Toast.LENGTH_LONG).show();
+                Toast.makeText(PeripheralsControlActivity.this, "Error connecting raspberry", Toast.LENGTH_LONG).show();
                 //Disable switches if no connection
                 mSwLed.setEnabled(false);
                 mTvLed.setEnabled(false);
@@ -129,6 +170,39 @@ public class PeripheralsControlActivity extends AppCompatActivity {
             switchLedAPI(1);
         } else {
             switchLedAPI(0);
+        }
+    }
+
+    public void switchESPLed(boolean state) {
+        Request req;
+        if (state) {
+            req = ledOnESP;
+        } else {
+            req = ledOffESP;
+        }
+
+        okClient.newCall(req).enqueue(new okhttp3.Callback() {
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(okhttp3.Call call, okhttp3.Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Error: " + response, Toast.LENGTH_LONG).show();
+                } else {
+                    //GOOD
+                }
+            }
+        });
+    }
+
+    public void onDirectESP8266LedSwitch(View view) {
+        if (mESPLEd.isChecked()) {
+            switchESPLed(true);
+        } else {
+            switchESPLed(false);
         }
     }
 }
